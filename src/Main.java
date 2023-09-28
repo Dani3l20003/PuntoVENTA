a1828fb8178fd3fad31a5e5461db1c696020f875
Diego Ortiz <2030282@upv.edu.mx>
2023-09-28 12:10:57 -0600

    Agregue clase Producto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Producto p1 = new Producto("Coca Cola", 1.5, 10);
+        p1.mostrarVentana();
     }
 }
